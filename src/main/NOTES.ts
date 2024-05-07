60b41bc837598ef8aa7d094d9effdea6b7e67593
Audrey Harmon <ah0556702@otc.edu>
2024-05-07 08:53:11 -0500

    Updating NOTES.ts

--- src/main/NOTES.ts
+++ src/main/NOTES.ts
@@ -3,16 +3,8 @@
 5. Add level selection pane or page
 6. Implement API logic for selecting different levels
 
-9. Increase readibility for guess text field
-10. Add logic for letters to be added at random each time the wrong guess is made
-
 12. Add pane at game over for record of scores
 
 13. When a book is guess correctly, populate the book with new data
-14. Style text field for user guess
-15. Style game over screen
-
-17. Style Score label in tab pane
-18. Add game play
 
 20.
